fdcb50d58ee7dc2192069e538103278cc136410d
Brooke <46959407+nimajnebec@users.noreply.github.com>
2026-03-12 22:22:40 +0000

    feat: make iframes persistant

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import Navbar from "./Navbar.svelte";
-
+	import { iframes } from "$lib";
+	import { page } from "$app/state";
 	import "greset";
 	import "$lib/style.scss";
 
 	let { children } = $props();
+	let loadedIframes = $state<Record<string, true>>({});
+
+	$effect(() => {
+		const activeIframeId = page.url.pathname.slice(1);
+		if (activeIframeId.length > 0) {
+			loadedIframes[activeIframeId] = true;
+		}
+	});
 </script>
 
 <svelte:head>
@@ -13,6 +22,16 @@
 
 <main>
 	<Navbar />
+
+	{#each iframes as iframe}
+		{#if loadedIframes[iframe.id] || page.url.pathname === `/${iframe.id}`}
+			<iframe
+				src={iframe.url}
+				title={iframe.label}
+				style:display={page.url.pathname === `/${iframe.id}` ? "block" : "none"}
+			></iframe>
+		{/if}
+	{/each}
 
 	{@render children()}
 </main>
@@ -23,5 +42,11 @@
 		flex-direction: column;
 		min-height: 100vh;
 		height: 100vh;
+	}
+
+	iframe {
+		border: none;
+		flex: 1;
+		width: 100%;
 	}
 </style>
--- src/routes/[iframe]/+page.svelte
+++ src/routes/[iframe]/+page.svelte
@@ -7,13 +7,3 @@
 <svelte:head>
 	<title>{PUBLIC_TITLE} - {data.iframe.label}</title>
 </svelte:head>
-
-<iframe src={data.iframe.url} title={data.iframe.label}></iframe>
-
-<style lang="scss">
-	iframe {
-		border: none;
-		height: 100%;
-		width: 100%;
-	}
-</style>
